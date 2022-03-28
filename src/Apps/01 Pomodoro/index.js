0606f9e8d4b7dd223fdb34a6258b46219cdb1113
Angel Higueros <99747585+angelhiguerosTelus@users.noreply.github.com>
2022-03-28 10:47:06 -0600

    feat: useLocalStorage hook implemented (#3)

--- src/Apps/01 Pomodoro/index.js
+++ src/Apps/01 Pomodoro/index.js
@@ -2,18 +2,22 @@ import React from "react";
 import { Timer } from "./timer";
 import { Logs } from "./logs";
 import "./styles.css";
+import { useLocalStorage } from "./useLocalStorage";
 
 function PomodoroApp() {
+  const [logs, setLogs] = useLocalStorage("timer", []);
+
+
   return (
     <div className="App box">
       <span className="subtitle is-3">Pomodoro App</span>
 
       <div className="columns is-centered mt-5">
         <div className="column is-4">
-          <Timer />
+          <Timer  logs={logs}  setLogs={setLogs}/>
         </div>
         <div className="column is-4">
-          <Logs />
+          <Logs logs={logs}/>
         </div>
       </div>
     </div>
